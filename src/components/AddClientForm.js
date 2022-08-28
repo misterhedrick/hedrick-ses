e8385102ecc5f464f27714a76734d765ba95c4de
Daniel Hedrick <misterhedrick@gmail.com>
2022-08-28 16:27:06 -0400

    start form

---
+++ src/components/AddClientForm.js
@@ -0,0 +1,11 @@
+import styles from '../styles/AddClientForm.module.scss';
+
+const AddClientForm = (props) => {
+  return (
+    <form>
+      <label for="name">Name</label>
+      <input id="name"></input>
+    </form>
+  );
+};
+export default AddClientForm;
