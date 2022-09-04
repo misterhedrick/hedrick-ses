8f6f3d4386f12438f27d3931500c0f62f81c001d
Daniel Hedrick <misterhedrick@gmail.com>
2022-09-04 07:10:52 -0400

    persisting login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthContextProvider } from "./store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <AuthContextProvider>
+    <BrowserRouter>
       <App />
-  </BrowserRouter>
+    </BrowserRouter>
+  </AuthContextProvider>
 );
